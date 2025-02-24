d8d816c30e017a6644af78e96baedc690f718ba8
Daniil <notprogramm@mail.ru>
2025-02-24 11:01:23 +0300

    Initial commit

---
+++ src/main/java/by/casino/luckytavern/LuckyTavernApplication.java
@@ -0,0 +1,13 @@
+package by.casino.luckytavern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckyTavernApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuckyTavernApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/casino/luckytavern/LuckyTavernApplicationTests.java
@@ -0,0 +1,13 @@
+package by.casino.luckytavern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckyTavernApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
